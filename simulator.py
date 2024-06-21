82c1a9de2236c29cd82bf8daf7e72ab7f5f49524
roy-asraf1 <snoopdog2796@gmail.com>
2024-06-21 20:18:54 +0300

    with commits

--- simulator.py
+++ simulator.py
@@ -20,73 +20,74 @@ max_time_seconds = 300  # Maximum time to reach the attacker in seconds (e.g., 5
 # Time since indication for the thread
 indication_time_seconds = 0
 
-def update_time_since_indication():
-    global indication_time_seconds
+def update_time_since_indication(): # Update the time since indication
+    global indication_time_seconds # Use the global variable
     while True:
-        indication_time_seconds += 1
-        time.sleep(1)
+        indication_time_seconds += 1 # Increment the time since indication
+        time.sleep(1) # Sleep for 1 second
 
-def start_calculations():
+def start_calculations(): # Start the calculations
     try:
-        x = float(miz_entry.get())
-        y = float(north_entry.get())
-        direction = float(direction_entry.get())
+        x = float(miz_entry.get()) # Get the x value
+        y = float(north_entry.get()) # Get the y value
+        direction = float(direction_entry.get()) # Get the direction value
     except ValueError:
-        result_label.config(text="Invalid input. Please enter numeric values.")
+        result_label.config(text="Invalid input. Please enter numeric values.") # Display an error message
         return
 
-    if x < MINIMUM or x > MAXIMUM or y < MINIMUM or y > MAXIMUM or direction < 0 or direction > 360:
-        result_label.config(text="There was an error, please try again.")
+    if x < MINIMUM or x > MAXIMUM or y < MINIMUM or y > MAXIMUM or direction < 0 or direction > 360: # Check if the values are out of range
+        result_label.config(text="There was an error, please try again.") # Display an error message
         return
 
-    z = 1500
-    first_location = Location(x, y, z)
-    direction_rad = math.radians(direction)
-    indication = Indication(first_location, direction)
+    z = 1500 # Set the z value to 1500
+    first_location = Location(x, y, z) # Create a new location object for the attacker
+    direction_rad = math.radians(direction) # Convert the direction to radians
+    indication = Indication(first_location, direction) # Create a new indication object
     attacker = Attacker(indication)  # Attacker object
 
-    with open('road.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['time', 'x', 'y', 'height'])
+    with open('road.csv', mode='w', newline='') as file: # Open the road.csv file
+        writer = csv.writer(file) # Create a CSV writer
+        writer.writerow(['time', 'x', 'y', 'height']) # Write the header row
 
-        time_seconds = 0
-        current_x = x
-        current_y = y
+        time_seconds = 0 # Set the time to 0
+        current_x = x # Set the current x value
+        current_y = y # Set the current y value
 
-        for _ in range(attacker.duration_seconds):
-            current_x += attacker.speed_mps * math.cos(direction_rad)
-            current_y += attacker.speed_mps * math.sin(direction_rad)
-            current_z = attacker.height
-            writer.writerow([time_seconds, current_x, current_y, current_z])
-            time_seconds += 1
+        for _ in range(attacker.duration_seconds): # Loop through the duration of the attacker
+            current_x += attacker.speed_mps * math.cos(direction_rad) # Update the x value
+            current_y += attacker.speed_mps * math.sin(direction_rad) # Update the y value
+            current_z = attacker.height # Set the z value to the attacker's height
+            writer.writerow([time_seconds, current_x, current_y, current_z]) 
+            time_seconds += 1 
 
     print("road.csv created successfully :)")
 
     # Start the thread to update the time since indication
-    time_thread = threading.Thread(target=update_time_since_indication)
-    time_thread.daemon = True
-    time_thread.start()
+    time_thread = threading.Thread(target=update_time_since_indication) # Create a new thread
+    time_thread.daemon = True # Set the thread as a daemon
+    time_thread.start() # Start the thread
 
     try:
-        x_myLocation = float(x_location_entry.get())
-        y_myLocation = float(y_location_entry.get())
-        height = float(height_entry.get())
+        x_myLocation = float(x_location_entry.get()) # Get the x value
+        y_myLocation = float(y_location_entry.get()) # Get the y value
+        height = float(height_entry.get()) # Get the height value
     except ValueError:
-        result_label.config(text="Invalid input. Please enter numeric values.")
+        result_label.config(text="Invalid input. Please enter correct values.")
         return
 
-    myLocation = Location(x_myLocation, y_myLocation, height)
-    myDrone = drone.drone(myLocation)
+    myLocation = Location(x_myLocation, y_myLocation, height) # Create a new location object for the drone
+    myDrone = drone.drone(myLocation) # Create a new drone object
 
-    def update_reach_status():
+    def update_reach_status(): # Update the reach status
         while True:
-            distances_between_att_drone = []
+            distances_between_att_drone = [] 
             angles_between_drone_to_att = []
+            angles_vertical_between_drone_to_att = []
             time_for_att = []
             can_reach = []
 
             with open('road.csv', mode='r') as file:
-                reader = csv.DictReader(file)
+                reader = csv.DictReader(file) 
                 for row in reader:
                     x_attacker = float(row['x'])
                     y_attacker = float(row['y'])
@@ -95,16 +96,22 @@ def start_calculations():
                     distance = math.sqrt((x_attacker - myLocation.x)**2 + 
                                          (y_attacker - myLocation.y)**2 + 
                                          (z_attacker - myLocation.z)**2)
-                    distances_between_att_drone.append(distance)
+                    distances_between_att_drone.append(distance) 
 
-                    dx = x_attacker - myLocation.x
+                    dx = x_attacker - myLocation.x 
                     dy = y_attacker - myLocation.y
-                    angle = math.degrees(math.atan2(dy, dx))
+                    angle_horizontal = math.degrees(math.atan2(dy, dx)) 
                     
-                    if angle < 0:
-                        angle += 360
+                    if angle_horizontal < 0:
+                        angle_horizontal += 360
 
-                    angles_between_drone_to_att.append(angle)
+                    angles_between_drone_to_att.append(angle_horizontal)
+
+                    dz = z_attacker - myLocation.z
+                    distance_horizontal = math.sqrt(dx**2 + dy**2)  # horizontal distance
+                    angle_vertical = math.degrees(math.atan2(dz, distance_horizontal)) # vertical angle
+
+                    angles_vertical_between_drone_to_att.append(angle_vertical)
 
                     time_required = distance / my_speed_mps
                     time_for_att.append(time_required)
@@ -118,14 +125,14 @@ def start_calculations():
 
             with open('results.csv', mode='w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(['time', 'distance', 'angle', 'time_required', 'remaining_time', 'can_reach'])
+                writer.writerow(['time', 'distance', 'angle_horizontal', 'angle_vertical', 'time_required', 'remaining_time', 'can_reach'])
 
                 time_seconds = 0
-                for distance, angle, time_required, reach in zip(distances_between_att_drone, angles_between_drone_to_att, time_for_att, can_reach):
+                for distance, angle_horizontal, angle_vertical, time_required, reach in zip(distances_between_att_drone, angles_between_drone_to_att, angles_vertical_between_drone_to_att, time_for_att, can_reach):
                     time_attacker_reaches_point = time_seconds
                     remaining_time = time_attacker_reaches_point - indication_time_seconds
                     reach = time_required <= remaining_time
-                    writer.writerow([time_seconds, distance, angle, time_required, remaining_time, reach])
+                    writer.writerow([time_seconds, distance, angle_horizontal, angle_vertical, time_required, remaining_time, reach])
                     time_seconds += 1
 
             time.sleep(1)
@@ -152,7 +159,7 @@ def display_possible_solutions():
     else:
         solutions_text = "Possible solutions (10 with at least 10 seconds remaining):\n"
         for solution in possible_solutions[:10]:
-            solutions_text += f"Time: {solution['time']}, Distance: {solution['distance']}, Angle: {solution['angle']}, Time Required: {solution['time_required']}, Remaining Time: {solution['remaining_time']}\n"
+            solutions_text += f"Time: {solution['time']}, Distance: {solution['distance']}, Angle degrees: {solution['angle_horizontal']}, Angle Vertical: {solution['angle_vertical']}, Time Required: {solution['time_required']}, Remaining Time: {solution['remaining_time']}\n"
         print(solutions_text)
         solutions_label.config(text=solutions_text)
 
@@ -174,10 +181,10 @@ def display_results():
                 with open('results.csv', mode='r') as file:
                     reader = csv.DictReader(file)
                     for row in reader:
-                        if int(row['remaining_time']) >= 20:
-                            results_text.insert('end', f"Time: {row['time']}, Distance: {row['distance']}, Angle: {row['angle']}, Time Required: {row['time_required']}, Remaining Time: {row['remaining_time']}\n", 'bold')
+                        if int(row['remaining_time']) >= 10:
+                            results_text.insert('end', f"Time: {row['time']}, Distance: {row['distance']}, Angle Horizontal: {row['angle_horizontal']}, Angle Vertical: {row['angle_vertical']}, Time Required: {row['time_required']}, Remaining Time: {row['remaining_time']}\n", 'bold')
                         else:
-                            results_text.insert('end', f"Time: {row['time']}, Distance: {row['distance']}, Angle: {row['angle']}, Time Required: {row['time_required']}, Remaining Time: {row['remaining_time']}\n")
+                            results_text.insert('end', f"Time: {row['time']}, Distance: {row['distance']}, Angle Horizontal: {row['angle_horizontal']}, Angle Vertical: {row['angle_vertical']}, Time Required: {row['time_required']}, Remaining Time: {row['remaining_time']}\n")
                 results_text.config(state='disabled')
             except tk.TclError:
                 break
@@ -214,7 +221,6 @@ miz_entry.grid(row=0, column=1, padx=10, pady=5, sticky='w')
 
 north_label = ttk.Label(attacker_frame, text="Attacker's North:")
 north_label.grid(row=1, column=0, padx=10, pady=5, sticky='e')
-north_entry = ttk.Entry(attacker_frame)
 north_entry = ttk.Entry(attacker_frame)
 north_entry.grid(row=1, column=1, padx=10, pady=5, sticky='w')
 
